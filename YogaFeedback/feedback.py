4794de09c50e0fbce813062c83ce7148095a5ead
ranadheer0607 <107311269+ranadheer0607@users.noreply.github.com>
2025-08-31 23:41:53 +0530

    Update feedback.py

--- YogaFeedback/feedback.py
+++ YogaFeedback/feedback.py
@@ -40,7 +40,7 @@ if not existing_data or existing_data[0] != expected_headers:
 st.title("Ranadheer's Yoga Class Feedback")
 # Display the image
 image_url = "https://tse1.explicit.bing.net/th/id/OIP.JjOepauBQe_WsFOwNQZXuQAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3"
-st.image(image_url, caption="🧘 Yoga Guru Patanjali", use_column_width=True)
+st.image(image_url, caption="🧘 Yoga Guru Patanjali", use_container_width=True)
 
 
 name = st.text_input("Your Name")
